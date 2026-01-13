16ebc55ccc2ac299245330eb469f5f91fb29c127
Lucy <lucarytri@gmail.com>
2026-01-13 21:27:12 +0000

    Added Timer and PrimaryButton components

---
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,6 @@
+
+export function PrimaryButton({ text } : { text : string }) {
+  return (
+    <button className="primary-button">{text}</button>
+  )
+}
---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,6 @@
+
+export function Timer() {
+  return (
+    <h1>20:00</h1>
+  )
+}
