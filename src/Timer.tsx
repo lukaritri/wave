ce18b792bf824bfc458798711173e9256a493adc
Lucy <lucarytri@gmail.com>
2026-01-14 21:00:22 +0000

    Implement timer states

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 
 export function Timer() {
+  const [state, changeState] = useState('idle');
+
+  function handleClick() {
+    changeState(s => {
+      if (s == 'idle') return 'running'
+      if (s == 'running') return 'paused'
+      return 'idle'
+    })
+  }
+
+  const buttonText = state == 'idle' ? 'start' : state == 'running' ? 'pause' : 'resume'
+
   return (
-    <h1>20:00</h1>
+    <>
+      <h1>20:00</h1>
+      <button onClick={handleClick}>{buttonText}</button>
+    </>
   )
 }
