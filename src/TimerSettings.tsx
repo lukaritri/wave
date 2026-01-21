751e5d7db13f2661941c2cdca01f00bd915e5572
Lucy <lucarytri@gmail.com>
2026-01-21 20:53:31 +0000

    Implement simple time component

--- src/TimerSettings.tsx
+++ src/TimerSettings.tsx
@@ -1,9 +1,23 @@
 import './TimerSettings.css'
+import UpArrow from './assets/up-arrow.svg?react'
+import DownArrow from './assets/down-arrow.svg?react'
+
+function TimeComponent() {
+  return (
+    <div className='timer-settings-unit'>
+      <UpArrow />
+      <div className='timer-settings-edit'>
+        00
+      </div>
+      <DownArrow />
+    </div>
+  )
+}
 
 export function TimerSettings() {
   return (
     <div className='timer-settings'>
-      20:00
+      <TimeComponent />
     </div>
   )
 }
