5d41027abe266abd681d1b9ff93f58344b3cec93
Lucy <lucarytri@gmail.com>
2026-01-21 20:23:15 +0000

    Create TimerSettings.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
-import { Timer } from './Timer'
+// import { Timer } from './Timer'
+import { TimerSettings } from './TimerSettings'
 
 function App() {
 
   return (
     <div className='app'>
 
-      <div className='panel'>
+      {/* <div className='panel'>
         <h1 className='brand'>wave</h1>
         <Timer />
-      </div>
+      </div> */}
+
+      <TimerSettings />
       
     </div>
   )
