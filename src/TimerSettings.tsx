be588c92fb68f8ec3848229c20698be512587ae4
lukaritri <lucarytri@gmail.com>
2026-01-22 20:31:58 +0000

    Implement timer component functionality

--- src/TimerSettings.tsx
+++ src/TimerSettings.tsx
@@ -1,23 +1,53 @@
 import './TimerSettings.css'
 import UpArrow from './assets/up-arrow.svg?react'
 import DownArrow from './assets/down-arrow.svg?react'
+import { useState } from 'react'
 
-function TimeComponent() {
+type TimeComponentProps = {
+  displayTime : number,
+  increment : () => void,
+  decrement : () => void
+}
+
+function TimeComponent({displayTime, increment, decrement}:TimeComponentProps) {
   return (
     <div className='timer-settings-unit'>
-      <UpArrow />
+
+      <button className='icon-button' onClick={increment}>
+        <UpArrow />
+      </button>
+      
       <div className='timer-settings-edit'>
-        00
+        {String(displayTime).padStart(2, '0')}
       </div>
-      <DownArrow />
+
+      <button className='icon-button' onClick={decrement}>
+        <DownArrow />
+      </button>
+      
     </div>
   )
 }
 
 export function TimerSettings() {
+
+  const [hours, setHours] = useState<number>(0)
+  const [minutes, setMinutes] = useState<number>(0)
+  const [seconds, setSeconds] = useState<number>(0)
+
   return (
     <div className='timer-settings'>
-      <TimeComponent />
+
+      <TimeComponent 
+        displayTime={hours}
+        increment={() => {
+          setHours(Math.min(hours + 1, 59))
+        }}
+        decrement={() => {
+          setHours(Math.max(hours - 1, 0))
+        }}
+      />
+      
     </div>
   )
 }
