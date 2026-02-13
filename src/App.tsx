fb323bc7f4239e73b138750f433a10f21856e4a2
Lucy Lau <lucarytri@gmail.com>
2026-02-13 11:50:28 +0000

    Prettier formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import './App.css'
-import { Timer } from './Timer'
-import { TimerSettings } from './TimerSettings'
-import { useState } from 'react'
+import './App.css';
+import { Timer } from './Timer';
+import { TimerSettings } from './TimerSettings';
+import { useState } from 'react';
 
 function App() {
-
-  const [showSettings, setShowSettings] = useState<boolean>(false)
+  const [showSettings, setShowSettings] = useState<boolean>(false);
   const [totalDurationSec, setTotalDurationSec] = useState<number>(() => {
-    const durationStr = localStorage.getItem('duration')
-    return durationStr ? parseInt(durationStr) : 25 * 60
-  })
+    const durationStr = localStorage.getItem('duration');
+    return durationStr ? parseInt(durationStr) : 25 * 60;
+  });
 
   function toggleSettings() {
-    setShowSettings(!showSettings)
+    setShowSettings(!showSettings);
   }
 
-  function updateTime(newTime : number) {
-    setTotalDurationSec(newTime)
-    localStorage.setItem('duration', String(newTime))
+  function updateTime(newTime: number) {
+    setTotalDurationSec(newTime);
+    localStorage.setItem('duration', String(newTime));
   }
 
   return (
-    <div className='app'>
-
-      <Timer 
+    <div className="app">
+      <Timer
         totalDurationSec={totalDurationSec}
         toggleSettings={toggleSettings}
       />
 
-      { showSettings ? <TimerSettings 
-                        totalDurationSec={totalDurationSec} 
-                        updateTime={updateTime} 
-                        toggleSettings={toggleSettings} /> : null }
-      
+      {showSettings ? (
+        <TimerSettings
+          totalDurationSec={totalDurationSec}
+          updateTime={updateTime}
+          toggleSettings={toggleSettings}
+        />
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,146 +1,152 @@
-import { useState, useEffect } from 'react'
-import type { TimerStatus } from './types/react'
-import './Timer.css'
+import { useState, useEffect } from 'react';
+import type { TimerStatus } from './types/react';
+import './Timer.css';
 
 /*
 Top-level variables
 */
 
 const primaryLabel: Record<TimerStatus, string> = {
-  idle : 'start',
-  running : 'pause',
-  paused : 'resume'
-}
+  idle: 'start',
+  running: 'pause',
+  paused: 'resume',
+};
 
 type PrimaryButtonProps = {
-  timerStatus : TimerStatus
-  onPrimaryClick : () => void
-}
+  timerStatus: TimerStatus;
+  onPrimaryClick: () => void;
+};
 
-const secondaryLabel: string = 'end'
+const secondaryLabel: string = 'end';
 
 type SecondaryButtonProps = {
-  timerStatus : TimerStatus
-  onSecondaryClick : () => void
-}
+  timerStatus: TimerStatus;
+  onSecondaryClick: () => void;
+};
 
-function PrimaryButton({timerStatus, onPrimaryClick} : PrimaryButtonProps) {
+function PrimaryButton({ timerStatus, onPrimaryClick }: PrimaryButtonProps) {
   return (
-    <button className='btn-primary' onClick={onPrimaryClick}>
+    <button className="btn-primary" onClick={onPrimaryClick}>
       {primaryLabel[timerStatus]}
     </button>
-  )
-  
+  );
 }
 
-function SecondaryButton({timerStatus, onSecondaryClick} : SecondaryButtonProps) {
-  if (timerStatus === 'idle') return null
+function SecondaryButton({
+  timerStatus,
+  onSecondaryClick,
+}: SecondaryButtonProps) {
+  if (timerStatus === 'idle') return null;
 
   return (
-    <button className='btn-secondary' onClick={onSecondaryClick}>
+    <button className="btn-secondary" onClick={onSecondaryClick}>
       {secondaryLabel}
     </button>
-  )
-  
+  );
 }
 
 type TimerProps = {
-  totalDurationSec : number,
-  toggleSettings : () => void
-}
+  totalDurationSec: number;
+  toggleSettings: () => void;
+};
 
-export function Timer({totalDurationSec, toggleSettings} : TimerProps) {
+export function Timer({ totalDurationSec, toggleSettings }: TimerProps) {
   /*
   Timer logic
   */
 
-  const [timerStatus, setTimerStatus] = useState<TimerStatus>('idle')
-  const [remainingSec, setRemainingSec] = useState<number>(totalDurationSec)
-  const [endTimeMs, setEndTimeMs] = useState<number | null>(null)
+  const [timerStatus, setTimerStatus] = useState<TimerStatus>('idle');
+  const [remainingSec, setRemainingSec] = useState<number>(totalDurationSec);
+  const [endTimeMs, setEndTimeMs] = useState<number | null>(null);
 
   // useEffect runs when page re-renders
   useEffect(() => {
-    if (endTimeMs === null) return
+    if (endTimeMs === null) return;
 
     const tick = () => {
       // calculating time left more reliable than setInterval every second
-      const timeLeftMs = endTimeMs - Date.now()
-      const timeLeftSec = Math.max(0, Math.ceil(timeLeftMs / 1000))
-      setRemainingSec(timeLeftSec)
-    }
+      const timeLeftMs = endTimeMs - Date.now();
+      const timeLeftSec = Math.max(0, Math.ceil(timeLeftMs / 1000));
+      setRemainingSec(timeLeftSec);
+    };
 
     /* I'm not a fan of how I setInterval then immediately clear it 
     if the state is not running, not sure how to improve it. */
-    const id = setInterval(tick, 250)
+    const id = setInterval(tick, 250);
 
     if (timerStatus === 'paused' || timerStatus === 'idle') {
-      clearInterval(id)
+      clearInterval(id);
     }
 
-    return () => clearInterval(id) // needed for when re-starting the timer
-  }, [endTimeMs, timerStatus])
+    return () => clearInterval(id); // needed for when re-starting the timer
+  }, [endTimeMs, timerStatus]);
 
   useEffect(() => {
-    setRemainingSec(totalDurationSec)
-  }, [totalDurationSec])
+    setRemainingSec(totalDurationSec);
+  }, [totalDurationSec]);
 
-  const remainingMin: number = Math.floor(remainingSec / 60)
+  const remainingMin: number = Math.floor(remainingSec / 60);
 
   /*
   Button click functions
   */
 
   function handlePrimaryClick() {
-    setTimerStatus(prev => {
+    setTimerStatus((prev) => {
       switch (prev) {
-        case 'idle' :
-          setEndTimeMs(Date.now() + totalDurationSec * 1000) // start timer
-          return 'running'
+        case 'idle':
+          setEndTimeMs(Date.now() + totalDurationSec * 1000); // start timer
+          return 'running';
 
-        case 'running' : 
-          return 'paused'
+        case 'running':
+          return 'paused';
 
-        case 'paused' : 
-          setEndTimeMs(Date.now() + remainingSec * 1000) // restart timer
-          return 'running'
+        case 'paused':
+          setEndTimeMs(Date.now() + remainingSec * 1000); // restart timer
+          return 'running';
       }
-    })
+    });
   }
 
   function handleSecondaryClick() {
     if (confirm('End session and restart timer?')) {
-      setTimerStatus('idle')
-      setEndTimeMs(null)
-      setRemainingSec(totalDurationSec)
+      setTimerStatus('idle');
+      setEndTimeMs(null);
+      setRemainingSec(totalDurationSec);
     } else {
       // treat confirmation dialog as if timer was paused and resumed
-      setEndTimeMs(Date.now() + remainingSec * 1000)
+      setEndTimeMs(Date.now() + remainingSec * 1000);
     }
   }
 
   return (
-    <div className='timer-page'>
+    <div className="timer-page">
+      <h1 className={timerStatus == 'idle' ? 'brand' : 'brand-running'}>
+        wave
+      </h1>
 
-      <h1 className={timerStatus == 'idle' ? 'brand' : 'brand-running'}>wave</h1>
-
-      <button className={timerStatus == 'idle' ? 'time-idle' : 'time-running'} style={timerStatus == 'idle' ? {} : {cursor: 'default'}} onClick={toggleSettings} disabled={timerStatus != 'idle'}>
+      <button
+        className={timerStatus == 'idle' ? 'time-idle' : 'time-running'}
+        style={timerStatus == 'idle' ? {} : { cursor: 'default' }}
+        onClick={toggleSettings}
+        disabled={timerStatus != 'idle'}
+      >
         {String(remainingMin).padStart(2, '0')}
-        <span className='colon'>:</span>
+        <span className="colon">:</span>
         {String(remainingSec % 60).padStart(2, '0')}
       </button>
 
-      <div className='controls'>
+      <div className="controls">
         <PrimaryButton
           timerStatus={timerStatus}
           onPrimaryClick={handlePrimaryClick}
         />
 
-        <SecondaryButton 
-          timerStatus={timerStatus} 
+        <SecondaryButton
+          timerStatus={timerStatus}
           onSecondaryClick={handleSecondaryClick}
         />
       </div>
-
     </div>
-  )
+  );
 }
